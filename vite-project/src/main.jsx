fdb7e1b68710e1250a2e2a0e7252b79fe8727e00
Jay Rupapara <92617593+JayRupapara@users.noreply.github.com>
2024-10-06 07:29:59 +0000

    updated setup

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { App } from './App';  // Import using curly braces
+import { App } from './App';  
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
